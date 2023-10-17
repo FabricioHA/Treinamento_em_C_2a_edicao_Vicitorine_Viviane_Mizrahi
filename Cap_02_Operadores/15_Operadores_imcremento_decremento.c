1a41964badc6e5d64af46780e55d008acfec3322
FabricioHA <57398120+FabricioHA@users.noreply.github.com>
2023-10-17 19:58:04 -0300

    Add files via upload

---
+++ Cap_02_Operadores/15_Operadores_imcremento_decremento.c
@@ -0,0 +1,51 @@
+/*15_Operadores_imcremento_decremento
+
+O operador de incremento ++ basicamente soma 1 ao valor
+recebido dá variável.
+
+x++  ==  x = x+1
+
+No fim, é uma forma de resumir o código, uma vez que essa
+operação é muito utilizada em loops.
+
+ela pode ser representada de duas formas:
+
+prefixada: ++x;
+pós-fixada: x++;
+
+nos dois casos, o efeito é o mesmo, mas a prefixada pode ser
+usada durante as operações, como por exemplo:
+
+n= 5;
+x= ++n;
+printf("\nN=%d   x+%d", n, x);
+
+saída:
+
+N=6   X=6
+
+A sintaxe de decremento é exatamente a mesma que incremento,
+com a diferença que o valor é subitraido em -1
+
+x = x-1;//Decrementa 1 de x
+
+equivalências
+--x; ou x--;
+
+
+
+cdigo de formatação         significado
+%c                           Caractere simples
+%d                           inteiro decimal com sinal
+%i                           inteirodecimal, hexadecimal ou octal
+%e                           notação ciênifica
+%f                           ponto flutuante em decimal
+%g                           Usa %e ou %f, o que for menor
+%o                           inteiro octal
+%s                           String de caracteres
+%u                           inteiro decimal sem sinal
+%x                           inteiro hexadecimal
+%ld                          inteiro decimal longo
+%lf                          ponto flutuante longo
+%Lf                          double longo
+*/
