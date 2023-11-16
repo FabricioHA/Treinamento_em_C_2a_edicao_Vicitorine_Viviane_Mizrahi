9c22106919eb680ed906303f8c1f8a15cc4a0acc
FabricioHA <57398120+FabricioHA@users.noreply.github.com>
2023-11-16 15:55:33 -0300

    Add files via upload

--- Cap_02_Operadores/Exercicios/EX_06.c
+++ Cap_02_Operadores/Exercicios/EX_06.c
@@ -12,17 +12,35 @@ int main()
 	setlocale(LC_ALL,""); //Exibir caracteres especiais corretamente
 	int a=1, b=2, c=3, d=4;
 	
-	//A)
-	printf("O valor de a += b+c é a = %i\n\n", a+=b+c);
+	//A) a += b + c
+	printf("a) O valor de a += b+c é a = %i\n\n", a+=b+c);
 	//Resultado: a = 6
 	a = 0;
 	a = 1;
 	
-	//B)
-	printf("O valor de b *= c = d + 2 é b = %i\n\n", b*=c= d + 2);
+	//B) b *= c = d + 2
+	printf("b)O valor de b *= c = d + 2 é b = %i\n\n", b*=c= d + 2);
+	//Resultado: a = 1
+	d = 0;
+	d = 4;
+	
+	//C) d %= a + a + a
+	printf("c) O valor de d %= a + a + a é d = %i\n\n", d %= a + a + a);
 	//Resultado: a = 12
 	b = 0;
 	b = 2;
+	
+	//D) d -= c -= b -= a
+	printf("d) O valor de d -= c -= b -= a é d = %i\n\n", d -= c -= b -= a);
+	//Resultado: a = -4
+	d = 0;
+	d = 4;
+	
+	//E) a += b += c += 7
+	printf("e) O valor de a += b += c += 7 é d = %i\n\n", a += b += c += 7);
+	//Resultado: a = 14
+	d = 0;
+	d = 4;
  	
 	system("PAUSE"); //Congelar tela nesse estado
 	return 0; //Retornar valor inteiro = 0
---
+++ Cap_02_Operadores/Exercicios/EX_07.c
@@ -0,0 +1,10 @@
+/*7_Os operadores relacionados são usados para:
+
+a)Combinar valores;
+b)comprar valores;
+c)distinguir diferentes tipos de variáveis;
+d)trocar variáveis por valores lógicos
+
+
+*/
+
