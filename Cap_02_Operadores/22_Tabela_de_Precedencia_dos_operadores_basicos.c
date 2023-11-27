ce0402a981959c1760c3cba0b0ec9e71b0d5cd11
FabricioHA <57398120+FabricioHA@users.noreply.github.com>
2023-11-27 15:51:37 -0300

    Add files via upload

---
+++ Cap_02_Operadores/22_Tabela_de_Precedencia_dos_operadores_basicos.c
@@ -0,0 +1,27 @@
+/*22_Tabela_de_Precedencia_dos_operadores_basicos
+
+Simbolo     operador
+-           Menos Unário
+++          Incremento prefixado ou pós-fixado
+--          Decremento prefixado ou pós-fixado
+!           Lógico NÃO
+*           Multiplicação aritimética
+/           Divisão Aritimética
+%           Módulo Aritimético
++           Mais aritimético
+-           Menos aritimético
+<           Menor relacional
+<=          menor ou igual relacional
+>           Maior relacional
+>=          maior ou igual relacional
+==          igual relacional
+!=          diferente relacional
+&&          E lógico
+!!          OU lógico
+?:          Condicional
+=           atribuição
+*=          Aritmético de atribuição (vezes)
+/=          Aritmético de atribuição (Divide)
+%=          Aritmético de atribuição (módulo)
++=          Aritmético de atribuição (soma)
+*/
---
+++ Cap_02_Operadores/23_operadores_e_a_Avaliacao_logica_ou_numerica.c
@@ -0,0 +1,14 @@
+/*23_operadores_e_a_Avaliacao_logica_ou_numerica
+
+Todo operador opera sobre um ou mais operandos, onde a operação por ele executada produz 
+um resultado. Os operandos são avaliados como numeros ou sinais lógicos, dependendo do
+operador, sendo assim o resultado aparecendo do mesmo jeito.
+
+Abaixo temos uma tabela referente a como funciona a geração de resultados entre operandos
+relacionais, aritiméticos e numéricos:
+
+Operadores     Operandos     Resultado
+Aritiméticos   Numéricos     Numéricos
+Relacionais    Numéricos     Lógicos
+Lógicos        Lógicos       Lógicos
+*/
