3cb92482cc7d47710399b5d5514591132091a3a7
FabricioHA <57398120+FabricioHA@users.noreply.github.com>
2023-11-29 15:04:06 -0300

    Add files via upload

--- Cap_02_Operadores/Exercicios/EX_12.c
+++ Cap_02_Operadores/Exercicios/EX_12.c
@@ -26,65 +26,136 @@ p)x<=5.0&&x!=1.0||i>j
 #include <stdlib.h> //Adicionar system()
 #include <locale.h> //Converter caracteres especiais
 #include <conio.h>  //Adicionar getch() e getche()
+#include <windows.h> //Adicionar opções graficas via windows
+
+#define WHITE "\x1B[0m" //Definir cor branca no texo de saída
 
 int main()
 {
+	ShowWindow(GetConsoleWindow(), SW_MAXIMIZE); //maximiza tela via identificador
+	
 	setlocale(LC_ALL, ""); //converter caracteres especiais
+	
+	system("title Capitulo 02, Exercicio 12"); //Definir titulo da tela de saída
+	system("mode con cols=11 lines 300"); //Ajustar tamanho da tela
+	
 	system("cls"); //Limpar tela
+	system("color 0A"); //Mudar cor de fundo e texto com system() do stdlib.h
 	
 	int i=1, j=2, k=3, n=2;
 	float x=3.3, y=4.4;
 	
 	//a) i < j + 3
-	printf("i=1, j=2, k=3, n=2\n\na)expressão i < j + 3 é %s\n\n", i < j + 3? "Verdadeiro": "falso");
+	printf("i=1, j=2, k=3, n=2, x=3.3, y=4.4\n\na)expressão i < j + 3 é %s\n\n", i < j + 3? "verdadeiro": "falso");
 	//Resultado: Verdadeiro
+	
+	i=0, j=0, k=0, n=0, x=0, y=0;;
+	i=1, j=2, k=3, n=2, x=3.3, y=4.4;;
 	
 	//b) 2 * i - 7 <= j - 8
-	printf("\nb)expressão 2 * i - 7 <= j - 8 é %s\n\n", 2 * i - 7 <= j - 8? "Verdadeiro": "falso");
-	//Resultado: Verdadeiro
+	printf("\nb)expressão 2 * i - 7 <= j - 8 é %s\n\n", 2 * i - 7 <= j - 8? "verdadeiro": "falso");
+	//Resultado: falso
+	
+	i=0, j=0, k=0, n=0, x=0, y=0;;
+	i=1, j=2, k=3, n=2, x=3.3, y=4.4;;
 	
 	//c) -x + y >= 2.0 * y
-	//Resultado: Verdadeiro
+	printf("\nc)expressão -x + y >= 2.0 * y é %s\n\n", -x + y >= 2.0 * y? "verdadeiro": "falso");
+	//Resultado: falso
+	
+	i=0, j=0, k=0, n=0, x=0, y=0;;
+	i=1, j=2, k=3, n=2, x=3.3, y=4.4;;
 	
 	//d) x == y
-	//Resultado: Verdadeiro
+	printf("\nd)expressão x == y é %s\n\n", x == y? "verdadeiro": "falso");
+	//Resultado: falso
+	
+	i=0, j=0, k=0, n=0, x=0, y=0;;
+	i=1, j=2, k=3, n=2, x=3.3, y=4.4;;
 	
 	//e) x != y
+	printf("\ne)expressão x != y é %s\n\n", x != y? "verdadeiro": "falso");
 	//Resultado: Verdadeiro
+	
+	i=0, j=0, k=0, n=0, x=0, y=0;;
+	i=1, j=2, k=3, n=2, x=3.3, y=4.4;;
 	
 	//f) i + j + k == -2 * -k
+	printf("\nf)expressão i + j + k == -2 * -k é %s\n\n", i + j + k == -2 * -k? "verdadeiro": "falso");
 	//Resultado: Verdadeiro
+	
+	i=0, j=0, k=0, n=0, x=0, y=0;;
+	i=1, j=2, k=3, n=2, x=3.3, y=4.4;;
 	
 	//g) !(n-j)
+	printf("\ng)expressão !(n-j) é %s\n\n", !(n-j)? "verdadeiro": "falso");
 	//Resultado: Verdadeiro
+	
+	i=0, j=0, k=0, n=0, x=0, y=0;;
+	i=1, j=2, k=3, n=2, x=3.3, y=4.4;;
 	
 	//h) !n-j
+	printf("\nh)expressão !n-j é %s\n\n", !n-j? "verdadeiro": "falso");
 	//Resultado: Verdadeiro
+	
+	i=0, j=0, k=0, n=0, x=0, y=0;;
+	i=1, j=2, k=3, n=2, x=3.3, y=4.4;;
 	
 	//i) !x*!x
-	//Resultado: Verdadeiro
+	printf("\ni)expressão !x*!x é %s\n\n", !x*!x? "verdadeiro": "falso");
+	//Resultado: falso
+	
+	i=0, j=0, k=0, n=0, x=0, y=0;;
+	i=1, j=2, k=3, n=2, x=3.3, y=4.4;;
 	
 	//j) i&&j&&k
+	printf("\nj)expressão i&&j&&k é %s\n\n", i&&j&&k? "verdadeiro": "falso");
 	//Resultado: Verdadeiro
+	
+	i=0, j=0, k=0, n=0, x=0, y=0;;
+	i=1, j=2, k=3, n=2, x=3.3, y=4.4;;
 	
 	//k) i||j-3&&0
+	printf("\nk)expressão i||j-3&&0 é %s\n\n", i||j-3&&0? "verdadeiro": "falso");
 	//Resultado: Verdadeiro
+	
+	i=0, j=0, k=0, n=0, x=0, y=0;;
+	i=1, j=2, k=3, n=2, x=3.3, y=4.4;;
 	
 	//l) i<j&&2>=k
-	//Resultado: Verdadeiro
+	printf("\nl)expressão i<j&&2>=k é %s\n\n", i<j&&2>=k? "verdadeiro": "falso");
+	//Resultado: falso
+	
+	i=0, j=0, k=0, n=0, x=0, y=0;;
+	i=1, j=2, k=3, n=2, x=3.3, y=4.4;;
 	
 	//m) i<j||2>=k
+	printf("\nm)expressão i<j||2>=k é %s\n\n", i<j||2>=k? "verdadeiro": "falso");
 	//Resultado: Verdadeiro
+	
+	i=0, j=0, k=0, n=0, x=0, y=0;;
+	i=1, j=2, k=3, n=2, x=3.3, y=4.4;;
 	
 	//n) i==2||j==4||k==5
-	//Resultado: Verdadeiro
+	printf("\nn)expressão i==2||j==4||k==5 é %s\n\n", i==2||j==4||k==5? "verdadeiro": "falso");
+	//Resultado: falso
+	
+	i=0, j=0, k=0, n=0, x=0, y=0;;
+	i=1, j=2, k=3, n=2, x=3.3, y=4.4;;
 	
 	//o) i=2||j==4||k==5
-	//Resultado: Verdadeiro
+	printf("\no)expressão i=2||j==4||k==5 é %s\n\n", -x + y >= 2.0 * y? "verdadeiro": "falso");
+	//Resultado: falso
+	
+	i=0, j=0, k=0, n=0, x=0, y=0;;
+	i=1, j=2, k=3, n=2, x=3.3, y=4.4;;
 	
 	//p)x<=5.0&&x!=1.0||i>j
+	printf("\np)expressão x<=5.0&&x!=1.0||i>j é %s\n\n", x<=5.0&&x!=1.0||i>j? "verdadeiro": "falso");
 	//Resultado: Verdadeiro
 	
+	i=0, j=0, k=0, n=0, x=0, y=0;;
+	i=1, j=2, k=3, n=2, x=3.3, y=4.4;;
 	
 	system("PAUSE"); //congelar operação do sistema
 	return 0; //se operação for verdadeira, retornar 0
